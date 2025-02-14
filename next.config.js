b6b9b15933d26f1041171a8d179263461e6dcbc9
Cendikia122 <cendikiabob@gmail.com>
2025-02-14 20:59:59 +0700

    ganti next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    unoptimized: true, // Mencegah error terkait Next.js Image Optimization
+  },
+  basePath: "/fasel-app", // Ganti dengan nama repository GitHub kamu
+  trailingSlash: true, // Menambahkan trailing slash agar routing lebih stabil
 };
 
 module.exports = nextConfig;
