ad678ade1d74ab747651a975173af2494c72d4e7
Cendikia122 <cendikiabob@gmail.com>
2025-02-14 21:23:08 +0700

    ganti remote

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "standalone",
+    images: {
+      unoptimized: true, // Mencegah error terkait Next.js Image Optimization
+    },
+    basePath: "/fasel-app", // Ganti dengan nama repository GitHub kamu
+    trailingSlash: true, // Menambahkan trailing slash agar routing lebih stabil
+  };
+  
+  module.exports = nextConfig;
+  
