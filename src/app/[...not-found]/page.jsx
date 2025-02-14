3fd4af27707306dbfa6e23bff6dc1af42dc5b34b
Cendikia122 <cendikiabob@gmail.com>
2025-02-14 17:23:30 +0700

    yaAllah

--- src/app/[...not-found]/page.jsx
+++ src/app/[...not-found]/page.jsx
@@ -4,21 +4,12 @@ import React from 'react';
 
 const NotFoundPage = () => {
     return (
-        <>
-            <LayoutStyle7 breadCrumb="404" title="404 Page">
-                <NotFoundContent />
-            </LayoutStyle7>
-        </>
+        <LayoutStyle7 breadCrumb="404" title="404 Page">
+            <NotFoundContent />
+        </LayoutStyle7>
     );
 };
 
-export function generateStaticParams() {
-    return [{ slug: "not-found" }]; // Static parameter untuk ekspor
-  }
-  
-  export const dynamic = "force-static";
+export const dynamic = "force-static";
 
-  export default function CustomNotFoundPage() { 
-    return <h1>Page Not Found</h1>;
-  }
-  
+export default NotFoundPage;
